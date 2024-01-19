833280786d9eb670cbfe0058ca7232f7766c73fe
Theoneste NDAYISENGA <ndayisengatheoneste79@gmail.com>
2024-01-19 11:38:13 +0200

    This is what Is going on in our life

--- python.py
+++ python.py
@@ -7,3 +7,7 @@ print("This is what I am learing to do, the next time")
 variable="This is my next name"
 print(variable[2:6})
 # this is another way of learning new things
+
+# I am happy that I have learned a new thing
+from linecache import error 
+print(f"This is what I was lookig for since this morning {variable}")
