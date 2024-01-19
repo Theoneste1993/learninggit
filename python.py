89d73bfdee14827320f5d4d0182dc342ca826291
Theoneste NDAYISENGA <55020646+Theoneste1993@users.noreply.github.com>
2024-01-19 09:33:03 +0200

    Update python.py

--- python.py
+++ python.py
@@ -3,3 +3,7 @@ import numpy as np
 # I will learn how to learn everything
 import matplotlib_inline as plt
 print("This is what I am learing to do, the next time")
+
+variable="This is my next name"
+print(variable[2:6})
+# this is another way of learning new things
